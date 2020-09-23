b0b77aff093dde302906345e5e109a0636b30bcc
Boris Edigarian <sys1988@gmail.com>
2020-09-23 17:45:04 +0400

    slider finished and social media icons added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,23 +1,36 @@
-// Slider animation
+// Swiper slider
+
+var mySwiper = new Swiper('.swiper-container', {
+  speed: 1200,
+  loop: true,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 8000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
+
+// Slider elements animation
 gsap.from('.slider__title', {
   opacity: 0,
-  duration: 2,
+  duration: 1,
   y: -70,
-  ease: 'back.inOut(1.7)',
+  delay: 0.2,
 });
 gsap.from('.slider__text', {
   opacity: 0,
-  duration: 2,
+  duration: 1,
   y: -70,
   delay: 0.7,
-  ease: 'back.inOut(1.7)',
 });
 gsap.from('.slider__slide .btn', {
   opacity: 0,
-  duration: 2,
+  duration: 1,
   y: -70,
   delay: 1.2,
-  ease: 'back.inOut(1.7)',
 });
 
 // Objects
@@ -25,7 +38,7 @@ gsap.from('.objects__1 .elem', {
   opacity: 0,
   duration: 3,
   y: -450,
-  delay: 1.6,
+  delay: 0.5,
   ease: 'back.inOut(1.7)',
 });
 
@@ -33,6 +46,6 @@ gsap.from('.objects__2 .elem', {
   opacity: 0,
   duration: 3,
   x: 810,
-  delay: 2,
+  delay: 1,
   ease: 'back.inOut(1.7)',
 });

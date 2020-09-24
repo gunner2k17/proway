65c2a9c22717d3512ecbf68f881e1d41fe86ff7d
Boris Edigarian <sys1988@gmail.com>
2020-09-24 20:29:27 +0400

    about section object reveal animation added

--- dist/js/app.js
+++ dist/js/app.js
@@ -58,19 +58,17 @@ gsap.from('.objects__2 .elem', {
 });
 
 // About section
-// let tl = gsap.timeline({
-//   // yes, we can add it to an entire timeline!
-//   scrollTrigger: {
-//     trigger: '.about__object',
-//     pin: true, // pin the trigger element while active
-//     start: 'top top', // when the top of the trigger hits the top of the viewport
-//     end: '+=500', // end after scrolling 500px beyond the start
-//     scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-//     snap: {
-//       snapTo: 'labels', // snap to the closest label in the timeline
-//       duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-//       delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-//       ease: 'power1.inOut', // the ease of the snap animation ("power3" by default)
-//     },
-//   },
-// });
+let about = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.about',
+    start: 'top bottom',
+  },
+});
+
+about.from('.about__object', {
+  opacity: 0,
+  duration: 2,
+  rotation: -45,
+  x: -810,
+  ease: 'back.inOut(1.7)',
+});

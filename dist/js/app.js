399e19a377bc958fd03459ec1591305ffc0edfed
Boris Edigarian <sys1988@gmail.com>
2020-10-05 00:23:48 +0400

    details and projects pages finished

--- dist/js/app.js
+++ dist/js/app.js
@@ -14,228 +14,246 @@ var mySwiper = new Swiper('.swiper-container', {
 });
 
 // Slider elements animation
-gsap.from('.slider__title', {
-  opacity: 0,
-  duration: 1,
-  y: -70,
-  delay: 0.2,
-});
+if (document.querySelector('.slider')) {
+  gsap.from('.slider__title', {
+    opacity: 0,
+    duration: 1,
+    y: -70,
+    delay: 0.2,
+  });
 
-gsap.from('.slider__text', {
-  opacity: 0,
-  duration: 1,
-  y: -70,
-  delay: 0.7,
-});
-gsap.from('.slider__slide .btn', {
-  opacity: 0,
-  duration: 1,
-  y: -70,
-  delay: 1.2,
-});
-gsap.from('.social', {
-  opacity: 0,
-  duration: 1.2,
-  y: -70,
-  delay: 2,
-  ease: 'back.inOut(1.7)',
-});
+  gsap.from('.slider__text', {
+    opacity: 0,
+    duration: 1,
+    y: -70,
+    delay: 0.7,
+  });
+  gsap.from('.slider__slide .btn', {
+    opacity: 0,
+    duration: 1,
+    y: -70,
+    delay: 1.2,
+  });
+  gsap.from('.social', {
+    opacity: 0,
+    duration: 1.2,
+    y: -70,
+    delay: 2,
+    ease: 'back.inOut(1.7)',
+  });
+}
 
 // Objects
-gsap.from('.objects__1 .elem', {
-  opacity: 0,
-  duration: 3,
-  y: -450,
-  delay: 0.5,
-  ease: 'back.inOut(1.7)',
-});
+if (document.querySelector('.objects')) {
+  gsap.from('.objects__1 .elem', {
+    opacity: 0,
+    duration: 3,
+    y: -450,
+    delay: 0.5,
+    ease: 'back.inOut(1.7)',
+  });
 
-gsap.from('.objects__2 .elem', {
-  opacity: 0,
-  duration: 3,
-  x: 810,
-  delay: 1,
-  ease: 'back.inOut(1.7)',
-});
+  gsap.from('.objects__2 .elem', {
+    opacity: 0,
+    duration: 3,
+    x: 810,
+    delay: 1,
+    ease: 'back.inOut(1.7)',
+  });
+}
 
 // About section
-let about = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.about',
-    start: 'top bottom',
-  },
-});
+if (document.querySelector('.about')) {
+  let about = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.about',
+      start: 'top bottom',
+    },
+  });
 
-about.from('.about__object', {
-  opacity: 0,
-  duration: 2,
-  x: -810,
-  ease: 'back.inOut(1.7)',
-});
+  about.from('.about__object', {
+    opacity: 0,
+    duration: 2,
+    x: -810,
+    ease: 'back.inOut(1.7)',
+  });
+}
 
 // Solutions section
-gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger);
-let tl = gsap.timeline();
-tl.pause();
+if (document.querySelector('.solutions')) {
+  gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger);
+  let tl = gsap.timeline();
+  tl.pause();
 
-ScrollTrigger.create({
-  trigger: '.solutions__list',
-  start: 'top bottom',
-  onEnter: () =>
-    setTimeout(() => {
-      tl.resume();
-    }, 2500),
-});
+  ScrollTrigger.create({
+    trigger: '.solutions__list',
+    start: 'top bottom',
+    onEnter: () =>
+      setTimeout(() => {
+        tl.resume();
+      }, 2500),
+  });
 
-tl.fromTo(
-  svgCircleProgressPath1,
-  0.4,
-  {
-    drawSVG: '0',
-  },
-  {
-    drawSVG: '0 20%',
-  }
-);
+  tl.fromTo(
+    svgCircleProgressPath1,
+    0.4,
+    {
+      drawSVG: '0',
+    },
+    {
+      drawSVG: '0 20%',
+    }
+  );
 
-tl.fromTo(
-  svgCircleProgressPath2,
-  0.6,
-  {
-    drawSVG: '0',
-  },
-  {
-    drawSVG: '0 40%',
-  }
-);
+  tl.fromTo(
+    svgCircleProgressPath2,
+    0.6,
+    {
+      drawSVG: '0',
+    },
+    {
+      drawSVG: '0 40%',
+    }
+  );
 
-tl.fromTo(
-  svgCircleProgressPath3,
-  0.8,
-  {
-    drawSVG: '0',
-  },
-  {
-    drawSVG: '0 60%',
-  }
-);
+  tl.fromTo(
+    svgCircleProgressPath3,
+    0.8,
+    {
+      drawSVG: '0',
+    },
+    {
+      drawSVG: '0 60%',
+    }
+  );
 
-tl.fromTo(
-  svgCircleProgressPath4,
-  1.0,
-  {
-    drawSVG: '0',
-  },
-  {
-    drawSVG: '0 80%',
-  }
-);
+  tl.fromTo(
+    svgCircleProgressPath4,
+    1.0,
+    {
+      drawSVG: '0',
+    },
+    {
+      drawSVG: '0 80%',
+    }
+  );
 
-tl.fromTo(
-  svgCircleProgressPath5,
-  1.2,
-  {
-    drawSVG: '0',
-  },
-  {
-    drawSVG: '0 100%',
-  }
-);
+  tl.fromTo(
+    svgCircleProgressPath5,
+    1.2,
+    {
+      drawSVG: '0',
+    },
+    {
+      drawSVG: '0 100%',
+    }
+  );
+}
 
 // Solutions
-let solutions = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.solutions',
-    start: 'top bottom',
-  },
-});
+if (document.querySelector('.solutions')) {
+  let solutions = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.solutions',
+      start: 'top bottom',
+    },
+  });
 
-solutions.from('.solutions__object', {
-  opacity: 0,
-  duration: 2,
-  x: 810,
-  ease: 'back.inOut(3)',
-});
+  solutions.from('.solutions__object', {
+    opacity: 0,
+    duration: 2,
+    x: 810,
+    ease: 'back.inOut(3)',
+  });
 
-solutions.from('.solutions__circle', {
-  opacity: 0,
-  duration: 2,
-  x: 810,
-  ease: 'back.inOut(3)',
-});
+  solutions.from('.solutions__circle', {
+    opacity: 0,
+    duration: 2,
+    x: 810,
+    ease: 'back.inOut(3)',
+  });
+}
 
 // Agile
-let agile = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.agile',
-    start: 'top bottom',
-  },
-});
+if (document.querySelector('.agile')) {
+  let agile = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.agile',
+      start: 'top bottom',
+    },
+  });
 
-agile.from('.agile__object', {
-  duration: 1.5,
-  rotate: -20,
-  ease: 'back.inOut(3)',
-});
+  agile.from('.agile__object', {
+    duration: 1.5,
+    rotate: -20,
+    ease: 'back.inOut(3)',
+  });
 
-agile.from('.agile__circle', {
-  opacity: 0,
-  duration: 2,
-  x: -810,
-  ease: 'back.inOut(3)',
-});
+  agile.from('.agile__circle', {
+    opacity: 0,
+    duration: 2,
+    x: -810,
+    ease: 'back.inOut(3)',
+  });
+}
 
-// Projscts
-let projects = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.projects',
-    start: 'top bottom',
-  },
-});
+// Projects
+if (document.querySelector('.projects')) {
+  let projects = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.projects',
+      start: 'top bottom',
+    },
+  });
 
-projects.from('.projects__object', {
-  opacity: 0,
-  duration: 2,
-  x: -810,
-  ease: 'back.inOut(1.7)',
-});
+  projects.from('.projects__object', {
+    opacity: 0,
+    duration: 2,
+    x: -810,
+    ease: 'back.inOut(1.7)',
+  });
+}
 
 // Team
-let team = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.team',
-    start: 'top bottom',
-  },
-});
+if (document.querySelector('.team')) {
+  let team = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.team',
+      start: 'top bottom',
+    },
+  });
 
-team.from('.team__object', {
-  opacity: 0,
-  duration: 2,
-  x: 810,
-  ease: 'back.inOut(1.7)',
-});
+  team.from('.team__object', {
+    opacity: 0,
+    duration: 2,
+    x: 810,
+    ease: 'back.inOut(1.7)',
+  });
 
-team.from('.team__circle', {
-  opacity: 0,
-  duration: 2,
-  x: 810,
-  ease: 'back.inOut(3)',
-});
+  team.from('.team__circle', {
+    opacity: 0,
+    duration: 2,
+    x: 810,
+    ease: 'back.inOut(3)',
+  });
+}
 
 // Footer
-let footer = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.footer',
-    start: 'top bottom',
-  },
-});
+if (document.querySelector('.footer')) {
+  let footer = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.footer',
+      start: 'top bottom',
+    },
+  });
 
-team.from('.footer__circle', {
-  opacity: 0,
-  duration: 2,
-  x: -410,
-  ease: 'back.inOut(1.7)',
-});
+  footer.from('.footer__circle', {
+    opacity: 0,
+    duration: 2,
+    x: -410,
+    ease: 'back.inOut(1.7)',
+  });
+}
 
 // Date IIFE
 {
@@ -270,23 +288,25 @@ $('.modal').click(function (e) {
 });
 
 // Custom file upload
-const actualBtn = document.getElementById('actual-btn');
 const uploadWrapper = document.querySelector('.form__upload');
-const removeFileBtn = document.querySelector('.form__remove');
-const fileChosen = document.getElementById('file-chosen');
+if (uploadWrapper) {
+  const actualBtn = document.getElementById('actual-btn');
+  const removeFileBtn = document.querySelector('.form__remove');
+  const fileChosen = document.getElementById('file-chosen');
 
-actualBtn.addEventListener('change', function () {
-  fileChosen.textContent = this.files[0].name;
-  uploadWrapper.classList.add('form__upload--uploaded');
-});
+  actualBtn.addEventListener('change', function () {
+    fileChosen.textContent = this.files[0].name;
+    uploadWrapper.classList.add('form__upload--uploaded');
+  });
 
-removeFileBtn.addEventListener('click', () => {
-  actualBtn.value = null;
-  fileChosen.textContent = 'Resume';
-  uploadWrapper.classList.remove('form__upload--uploaded');
-});
+  removeFileBtn.addEventListener('click', () => {
+    actualBtn.value = null;
+    fileChosen.textContent = 'Resume';
+    uploadWrapper.classList.remove('form__upload--uploaded');
+  });
+}
 
-$(window).on('scroll', function () {
+$(document).on('scroll', function () {
   let scrollAmount = window.scrollY;
   if (scrollAmount > 0) {
     $('.header').addClass('header--white');
